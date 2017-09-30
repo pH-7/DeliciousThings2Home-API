54d4a56e82d6ea2de8e4170dfaa89e707dda9ff9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-09-30 15:37:32 -0700

    Add SignUp page

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -7,10 +7,12 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^restaurant/sign-in/$', auth_views.login,
-        {'template_name': 'restaurant/sign_in.html'},
+        {'template_name': 'restaurant/sign-in.html'},
         name='restaurant-sign-in'),
     url(r'^restaurant/sign-out/$', auth_views.logout,
         {'next_page': '/'},
         name='restaurant-sign-out'),
+    url(r'^restaurant/sign-up/$', views.restaurant_sign_up,
+            name='restaurant-sign-up'),
     url(r'^restaurant/$', views.restaurant_home, name='restaurant-home'),
 ]
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return redirect(restaurant_home)
 
-@login_required(login_url="/restaurant/sign-in/")
+@login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html')
+    return render(request, 'restaurant/home.html', {})
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign-up.html', {})
