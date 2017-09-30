134f7962f4c2d10bb59ee0790079b2058f9997f1
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-09-30 15:25:04 -0700

    Add few needed fields into models

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Restaurant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='restaurant')
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=80)
+    address = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to='restaurant_logo/', blank=False)
+
+    def __str__(self):
+        return self.name
