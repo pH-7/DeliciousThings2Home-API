854d5afc3ac4cdf7c264e67ae2192cc37729eb4d
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-01 12:52:23 -0700

    Setup PostgreSQL for Heroku

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,6 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+dj_database_url.config()
+DATABASE['default].update(db_from_env)
