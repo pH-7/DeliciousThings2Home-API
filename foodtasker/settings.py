0b1d1069f31ed004046f534da45684c3b9c5542a
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-01 13:13:25 -0700

    db_from_env wasnt assigned

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -128,5 +128,5 @@ LOGIN_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-dj_database_url.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
