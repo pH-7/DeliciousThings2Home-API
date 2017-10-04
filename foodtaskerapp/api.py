449561b5314c7c16762e6766ddf8ef90899cd0b5
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-04 13:34:37 -0700

    Add API endpoint to get meals

--- foodtaskerapp/api.py
+++ foodtaskerapp/api.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
-from foodtaskerapp.models import Restaurant
-from foodtaskerapp.serializer import RestaurantSerializer
+from foodtaskerapp.models import Restaurant, Meal
+from foodtaskerapp.serializer import RestaurantSerializer, MealSerializer
 
 def customer_get_restaurants(request):
     restaurants = RestaurantSerializer(
@@ -12,8 +12,14 @@ def customer_get_restaurants(request):
 
     return JsonResponse({'restaurants': restaurants})
 
-def customer_get_meals(request):
-    return JsonResponse({})
+def customer_get_meals(request, restaurant_id):
+    meals = MealSerializer(
+        Meal.objects.filter(restaurant_id = restaurant_id).order_by('-id'),
+        many = True,
+        context = {'request': request}
+    ).data
+
+    return JsonResponse({'meals': meals})
 
 def customer_add_order(request):
     return JsonResponse({})
