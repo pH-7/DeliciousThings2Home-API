a13fc34146b50cf604f279ad5ce4113602dc98b2
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-21 21:45:22 -0700

    Store FB graph avatar URL into constant (variable)

--- foodtaskerapp/social_auth_pipeline.py
+++ foodtaskerapp/social_auth_pipeline.py
@@ -1,8 +1,10 @@
 from foodtaskerapp.models import Customer, Driver
 
+FB_GRAPH_AVATAR_URL = 'https://graph.facebook.com/%s/picture?type=large'
+
 def create_user_by_type(backend, user, request, response, *arg, **kwargs):
     if backend.name == 'facebook':
-        avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
+        avatar = FB_GRAPH_AVATAR_URL % response['id']
 
     if request['user_type'] == 'driver' and not Driver.objects.filter(user_id=user.id):
         Driver.objects.create(user_id=user.id, avatar=avatar)
